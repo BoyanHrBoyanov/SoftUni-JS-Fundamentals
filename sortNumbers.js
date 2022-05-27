6bb6b34dc54a8d52290633afa0bf65a3b221fadc
BoyanHrBoyanov <106339703+BoyanHrBoyanov@users.noreply.github.com>
2022-05-27 14:50:26 +0300

    Delete sortNumbers.js

--- sortNumbers.js
+++
@@ -1,9 +0,0 @@
-function sortNumbers(a, b, c) {
-    let nums = [a, b, c];
-    let sorted = nums.sort(function(a, b){return a-b});
-
-    console.log(sorted[2]);
-    console.log(sorted[1]);
-    console.log(sorted[0]);
-}
-sortNumbers(-2, 1, 3)
