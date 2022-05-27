621afa88a400456f4c86a3e8ee4c83f64c46a932
BoyanHrBoyanov <106339703+BoyanHrBoyanov@users.noreply.github.com>
2022-05-27 14:50:11 +0300

    Delete nextDate.js

--- nextDate.js
+++
@@ -1,9 +0,0 @@
-function nextDate(year, month, day) {
-    let realMonth = month - 1;
-    let realDay = day + 1;
-    let other = new Date(year, realMonth, realDay).toISOString().split('T')[0].replace(/\b0/g, '');
-
-    console.log(other);
-
-}
-nextDate(2016, 9, 30)
